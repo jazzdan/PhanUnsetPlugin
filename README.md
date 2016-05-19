da8693aeb72e1fcc746c2b7201f79277514d6dd6
dmiller <dmiller@etsy.com>
2016-05-19 20:29:09 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# PhanUnsetPlugin
+
+Intrinsics, such as `unset` look like functions, but they are really expressions disguised as functions. You can pass undefined variables, etc.
+
+There's no reason to user `unset`. You can just set the variable to `null` to achieve the same effect.
---
+++ src/UnsetPlugin.php
@@ -0,0 +1,82 @@
+<?php declare (strict_types = 1);
+
+use ast\Node;
+use Phan\AST\AnalysisVisitor;
+use Phan\CodeBase;
+use Phan\Language\Context;
+use Phan\Plugin;
+use Phan\Plugin\PluginImplementation;
+
+class UnsetPlugin extends PluginImplementation {
+    /**
+     * @param CodeBase $code_base
+     * The code base in which the node exists
+     *
+     * @param Context $context
+     * The context in which the node exits. This is
+     * the context inside the given node rather than
+     * the context outside of the given node
+     *
+     * @param Node $node
+     * The php-ast Node being analyzed.
+     *
+     * @param Node $node
+     * The parent node of the given node (if one exists).
+     *
+     * @return void
+     */
+    public function analyzeNode(
+        CodeBase $code_base,
+        Context $context,
+        Node $node,
+        Node $parent_node = null
+    ) {
+        (new UnsetVisitor($code_base, $context, $this))(
+            $node
+        );
+    }
+}
+
+class UnsetVisitor extends AnalysisVisitor {
+    /** @var Plugin */
+    private $plugin;
+
+    public function __construct(
+        CodeBase $code_base,
+        Context $context,
+        Plugin $plugin
+    ) {
+        parent::__construct($code_base, $context);
+
+        $this->plugin = $plugin;
+    }
+
+    /**
+     * Default visitor that does nothing
+     *
+     * @param Node $node
+     * A node to analyze
+     *
+     * @return void
+     */
+    public function visit(Node $node) {
+    }
+
+    /**
+     * @param Node $node
+     * A node to analyze
+     *
+     * @return void
+     */
+    public function visitUnset(Node $node) {
+        $this->plugin->emitIssue(
+            $this->code_base,
+            $this->context,
+            'PhanPluginUnset',
+            '`unset` is not allowed. Set the variable to null to achieve the same effect'
+        );
+    }
+
+}
+
+return new UnsetPlugin();
